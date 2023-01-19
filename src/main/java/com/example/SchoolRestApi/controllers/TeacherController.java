4e5e2c68b89dfcc1d0ca73d635036c6015347bcb
SantiagoVelasquezMontoya <santiago.velasquez@ias.com.co>
2023-01-19 18:44:57 -0500

    teacher implementation methods

---
+++ src/main/java/com/example/SchoolRestApi/controllers/TeacherController.java
@@ -0,0 +1,14 @@
+package com.example.SchoolRestApi.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teacher")
+public class TeacherController {
+
+
+
+
+}
--- src/main/java/com/example/SchoolRestApi/services/implementation/ITeacherImpl.java
+++ src/main/java/com/example/SchoolRestApi/services/implementation/ITeacherImpl.java
@@ -1,31 +1,67 @@
 package com.example.SchoolRestApi.services.implementation;
 
 import com.example.SchoolRestApi.dto.TeacherDTO;
+import com.example.SchoolRestApi.repository.ITeacherRepository;
+import com.example.SchoolRestApi.repository.entity.Alumn;
+import com.example.SchoolRestApi.repository.entity.Teacher;
 import com.example.SchoolRestApi.services.ITeacherService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ITeacherImpl implements ITeacherService {
 
+
+    public final ITeacherRepository teacherRepository;
+
+    public ITeacherImpl(ITeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
+
     @Override
     public String save(TeacherDTO teacher) {
-        return null;
+        Optional<Teacher> testTeacher = teacherRepository.findById(teacher.getId());
+        if(testTeacher.isPresent()){
+            return "Teacher Already Exists";
+        } else{
+            teacherRepository.save(new Teacher(teacher));
+            return "Teacher Successfully Saved";
+        }
     }
 
     @Override
     public String update(TeacherDTO teacher) {
-        return null;
+        Optional<Teacher> targetTeacher = teacherRepository.findById(teacher.getId());
+        if(targetTeacher.isPresent()){
+            teacherRepository.save(new Teacher(teacher));
+            return "Teacher was successfully Updated";
+        } else{
+            return "No Teacher was found";
+        }
     }
 
     @Override
     public String delete(TeacherDTO teacher) {
-        return null;
+        Optional<Teacher> targetTeacher = teacherRepository.findById(teacher.getId());
+        if(targetTeacher.isPresent()){
+            teacherRepository.deleteById(teacher.getId());
+            return "Teacher was successfully Deleted";
+        } else{
+            return "No Teacher was found";
+        }
+
     }
 
     @Override
     public List<TeacherDTO> getAll() {
-        return null;
+        List<Teacher> teacherList = (List<Teacher>)teacherRepository.findAll();
+        return teacherList
+                .stream()
+                .map(TeacherDTO::new)
+                .collect(Collectors.toList());
+
     }
 }
