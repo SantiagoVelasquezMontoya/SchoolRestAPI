0b1d7afd0b97d7a0ed2dc642b833ce2cdb091c04
SantiagoVelasquezMontoya <santiago.velasquez@ias.com.co>
2023-01-19 19:48:25 -0500

    assignature service and implementation initialize

---
+++ src/main/java/com/example/SchoolRestApi/services/IAssignatureService.java
@@ -0,0 +1,24 @@
+package com.example.SchoolRestApi.services;
+
+import com.example.SchoolRestApi.dto.AlumnDTO;
+import com.example.SchoolRestApi.dto.AssignatureDTO;
+import com.example.SchoolRestApi.dto.TeacherDTO;
+import org.apache.catalina.User;
+
+import java.util.List;
+
+public interface IAssignatureService {
+
+    String saveAssignature(AssignatureDTO assignature);
+    String deleteAssignature(AssignatureDTO assignature);
+    String updateAssignature(AssignatureDTO assignature);
+    List<AssignatureDTO> getAssignatures();
+    String saveTeacher(TeacherDTO teacher);
+    TeacherDTO getTeacher();
+    String deleteTeacher(TeacherDTO teacher);
+    String saveUser(AlumnDTO alumn);
+    List<AlumnDTO> getAlumns();
+    String deleteAlumn(AlumnDTO alumn);
+
+
+}
